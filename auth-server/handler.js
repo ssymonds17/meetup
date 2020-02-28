f3f4ba8d377ff7fc0859683bb60ce380f1c71f33
Sam Symonds <samuelsymonds2017@gmail.com>
2020-02-28 18:01:58 +0000

    4.2 auth-server files added

---
+++ auth-server/handler.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const axios = require('axios');
+
+module.exports.getAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=ldc9mb8idsbqe85fu26gf8e5va'
+    + '&client_secret=hvqm3mb2l830d5v2q3v3r4a57t'
+    + '&grant_type=authorization_code'
+    + '&redirect_uri=https://ssymonds17.github.io/meetup/'
+    + '&code=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + 'client_id=ldc9mb8idsbqe85fu26gf8e5va'
+    + '&client_secret=hvqm3mb2l830d5v2q3v3r4a57t'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.refresh_token;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};

3035d1e3721885edb0386f7efd48c25fee2a2b6a
Sam Symonds <samuelsymonds2017@gmail.com>
2020-03-27 17:46:03 +0000

    Updated api.js for offline use
    This commit updates the code within the getEvents function to store the list of events in localStorage and adds a line to check whether the user is online or not

--- src/api.js
+++ src/api.js
@@ -42,6 +42,11 @@ async function getEvents(lat, lon, page) {
   return mockEvents.events;
  }
 
+ if (!navigator.onLine) {
+  const events = localStorage.getItem('lastEvents');
+  return JSON.parse(events);
+ }
+
  const token = await getAccessToken();
 
  if (token) {
@@ -58,7 +63,11 @@ async function getEvents(lat, lon, page) {
    url += '&lat=' + lat + '&lon=' + lon + '&page=' + page;
   }
   const result = await axios.get(url);
-  return result.data.events;
+  const events = result.data.events;
+  if (events.length) { // Checks if the events exist
+   localStorage.setItem('lastEvents', JSON.stringify(events));
+  }
+  return events;
  }
 }
 

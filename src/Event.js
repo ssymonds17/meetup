5ef32c833a7c01709fc7df30d656675cc3d88c71
Sam Symonds <samuelsymonds2017@gmail.com>
2020-04-22 16:04:47 +0100

    Implemented pie chart to Events.js
    This commit implements a pie chart to display the slot availability for the event as well as the slot limit.

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { PieChart, Pie, Legend, Tooltip, Cell, ResponsiveContainer } from 'recharts';
 
 class Event extends Component {
  state = {
   showDetails: false,
+  events: []
  }
 
  handleShowDetails = () => {
@@ -15,6 +17,13 @@ class Event extends Component {
 
  render() {
   const showDetails = this.state.showDetails;
+  const event = this.props.event;
+  const data = [
+   { name: "Attending", value: event.yes_rsvp_count },
+   { name: "Spaces Available", value: (event.rsvp_limit - event.yes_rsvp_count) }
+  ];
+  const colors = ["#8884d8", "#37c0ba"];
+
 
   return (
    <div className="Event">
@@ -28,6 +37,22 @@ class Event extends Component {
      <p className="EventTime">
       {this.props.event.local_time}
      </p>
+     {event.rsvp_limit &&
+      <ResponsiveContainer height={150} width={250}>
+       <PieChart>
+        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={32} label >
+         {
+          data.map((entry, index) => (<Cell key={`cell-${index}`} fill={colors[index]} />))
+         }
+        </Pie>
+        <Legend iconSize={10} iconType="circle" layout="horizontal" verticalAlign="bottom" align="center" />
+        <Tooltip />
+       </PieChart>
+      </ResponsiveContainer>
+     }
+     {!event.rsvp_limit &&
+      <p>{event.yes_rsvp_count} people coming</p>
+     }
      {!showDetails &&
       <button className="details-btn" onClick={this.handleShowDetails}>Show More</button>
      }
@@ -37,9 +62,8 @@ class Event extends Component {
     </div>
     {showDetails &&
      <div className="EventDescriptionContainer">
-      <p className="EventDescription">
-       {this.props.event.description}
-      </p>
+      <p className="EventDescription" dangerouslySetInnerHTML={{ __html: this.props.event.description }} />
+      <p><a href={this.props.event.link} target="_blank">Attend this meetup</a></p>
      </div>
     }
    </div>
